1ccd9622e2a48729329e844ea9573b89e7ddd564
Ignacio <ignacio.ramosgutierrez@uam.es>
2023-03-24 11:18:43 +0100

    data and documentation git push

---
+++ R/data.R
@@ -0,0 +1,25 @@
+
+
+#' participation.table
+#'
+#' Example data to create participation certificates
+#'
+#' @format A data frame
+#'
+#' \describe{
+#' A data frame with 4 rows and 5 columns
+#' }
+
+
+
+
+#' attendance.table
+#'
+#' Example data to create attendance certificates
+#'
+#' @format A data frame
+#'
+#' \describe{
+#' A data frame with 4 rows and 1 column
+#' }
+
