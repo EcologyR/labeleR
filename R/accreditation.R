9f0e58bee06be84533ab7b369c9079766e805e59
Francisco Rodriguez-Sanchez <f.rodriguez.sanc@gmail.com>
2023-05-19 12:50:56 +0200

    revise create_accreditation

--- R/accreditation.R
+++ R/accreditation.R
@@ -1,117 +1,123 @@
-#' Function to create accreditation cards (8 per DIN-A4 page)
+#' Create accreditation cards (8 per DIN-A4 page)
 #'
-#' @param data a data frame including names and affiliations (optional if \code{affiliation.column} is NULL) to create accreditation cards.
-#' @param path Folder path where the output will be printed
-#' @param event Title of the event
-#' @param name.column Column name of the \code{data} data frame which specifies the participant's name.
-#' @param affiliation.column Column name of the \code{data} data frame which specifies the participant's affiliation.
-#' @param lpic PNG object route. File route of the top-left image. Can be blank if set to NULL.
-#' @param rpic PNG object route. File route of the top-right image. Can be blank if set to NULL.
+#' @param data a data frame including names and (optionally) affiliations.
+#' @param path Character. Path to folder where the PDF file will be saved.
+#' @param event Character. Title of the event.
+#' @param name.column Character. Name of the column in `data` storing participants' name.
+#' @param affiliation.column Character (optional). Name of the column in `data`
+#' storing participants' affiliation.
+#' @param lpic Character (optional) Path to a PNG image to be located in the badge top-left.
+#' @param rpic Character (optional) Path to a PNG image to be located in the badge top-right.
 #'
-#' @return An 'output' folder with the PDF documents inside
+#' @return A PDF file is saved on disk.
 #'
 #' @export
 #'
 #' @author Julia G. de Aledo, Ignacio Ramos-Gutierrez
 #'
-#' @examples
-#' \dontrun{
-#' data <- read_sheet(url='https://docs.google.com/spreadsheets/d
+#' @examplesIf interactive()
+#' data <- read_sheet(url = 'https://docs.google.com/spreadsheets/d
 #'         /16smXdP-Ehwu1cEmJTbJI1DerIpUrOcD7H5Ni6z9B07M/edit#gid=0')
-#' create_accreditation(data=data,
-#' path = "LabeleR_output",
-#' event="INTERNATIONAL CONFERENCE OF MUGGLEOLOGY",
+#' create_accreditation(
+#' data = data,
+#' path = "labeleR_output",
+#' event = "INTERNATIONAL CONFERENCE OF MUGGLEOLOGY",
 #' name.column = "List",
-#' affiliation.column="Affiliation",
+#' affiliation.column = "Affiliation",
 #' lpic = NULL,
 #' rpic = NULL)
-#' }
-create_accreditation <- function(data=NULL,
-                                 path=NULL,
-                                 event=NULL,
-                                 name.column=NULL,
-                                 affiliation.column=NULL,
-                                 lpic=NULL,
-                                 rpic=NULL){
 
-  if(is.null(data)){
-    stop(" a 'data' data.frame must be provided.
-         To import from Google Sheets use function 'read_sheet()'")
+create_accreditation <- function(data = NULL,
+                                 path = NULL,
+                                 event = NULL,
+                                 name.column = NULL,
+                                 affiliation.column = NULL,
+                                 lpic = NULL,
+                                 rpic = NULL
+) {
+
+  if (is.null(data)) {
+    stop("A data.frame must be provided. To import from Google Sheets use function 'read_sheet()'")
   }
 
-  if(class(data)!="data.frame"){stop("The 'data' object must be a data frame.")}
+  if (!inherits(data, "data.frame")) {stop("The 'data' object must be a data frame.")}
 
-  if(is.null(path)){stop("A folder path must be specified.")}
-  if(!file.exists(path)){message("The specified folder does not exist. Creating folder")
-    dir.create(path)}
+  if (is.null(path)) {stop("A folder path must be specified.")}
+  if (!file.exists(path)) {
+    message("The specified folder does not exist. Creating folder")
+    dir.create(path)
+  }
 
-  if(is.null(event)){
+  if (is.null(event)) {
     message("No event provided")
-    event <- ""}
-  if(!(name.column)%in%colnames(data)){
-    stop("Column '", name.column ,
+    event <- ""
+  }
+
+  if (!(name.column) %in% colnames(data)) {
+    stop("Column '", name.column,
          " is not a column of your 'data' object. Please select from \n",
-         paste0("-", colnames(data), sep="\n"))
-    }
-  if(is.null(affiliation.column)){
+         paste0("-", colnames(data), sep = "\n"))
+  }
+
+  if (is.null(affiliation.column)) {
     affiliation.column <- ""
-    }
-  if(!(affiliation.column)%in%c(colnames(data),"")){
+  }
+
+  if (!(affiliation.column) %in% c(colnames(data),"")) {
     stop("Column '", affiliation.column ,
          "' is not a column of your 'data' object. Please select from \n",
-         paste0("-", colnames(data), sep="\n"))
-    }
-
-
-  if(!dir.exists("tmp")){
-    dir.create("tmp")
-  }
-  # if(!dir.exists("output")){dir.create("output")}
-  # erase.lpic <- F
-  # erase.rpic <- F
-  if(is.null(lpic)){
-    grDevices::png("tmp/blank.png", 150, 150, "px")
-    graphics::plot.new()
-    grDevices::dev.off()
-    lpic <- "tmp/blank.png"
-    erase.lpic<-T
-    }
-  if(is.null(rpic)){
-    grDevices::png("tmp/blank.png", 150, 150, "px")
-    graphics::plot.new()
-    grDevices::dev.off()
-    rpic <- "tmp/blank.png"
-    erase.rpic<-T
+         paste0("-", colnames(data), sep = "\n"))
   }
 
-
-  file.copy(lpic, "tmp/lpic.png", overwrite = T)#create files to call them lpic@rpic to make it homogeneous
-  file.copy(rpic, "tmp/rpic.png", overwrite = T)#create files to call them lpic@rpic to make it homogeneous
-   tmpl_file   <- system.file("rmarkdown/templates/accreditation/skeleton/skeleton.Rmd", package="labeleR")
-   file.copy(tmpl_file, "tmp/accreditation.Rmd", overwrite = T)#create files to call them lpic@rpic to make it homogeneous
+  # Create tempdir to store all intermediate files
+  tmp <- tempdir()
 
 
+  if (!is.null(lpic)) {
+    if (!file.exists(lpic)) {
+      stop(lpic, " file not found")
+    } else {
+      file.copy(from = lpic, to = file.path(tmp, "lpic.png"), overwrite = TRUE)
+    }
+  }
+
+  if (!is.null(rpic)) {
+    if (!file.exists(rpic)) {
+      stop(rpic, " file not found")
+    } else {
+      file.copy(from = rpic, to = file.path(tmp, "rpic.png"), overwrite = TRUE)
+    }
+  }
+
+  if (is.null(lpic)) {
+    grDevices::png(file.path(tmp, "lpic.png"), 150, 150, "px")
+    graphics::plot.new()
+    grDevices::dev.off()
+  }
+
+  if (is.null(rpic)) {
+    grDevices::png(file.path(tmp, "rpic.png"), 150, 150, "px")
+    graphics::plot.new()
+    grDevices::dev.off()
+  }
 
 
-  tmpl_file   <- "tmp/accreditation.Rmd"
-  out.name <- paste0("Accreditations")
-  output_file <- paste0(out.name,'.pdf')
-
-  # if(file.exists(paste0("output/",output_file))){message("Accreditation file already exists. Overwriting.")}
+  tmpl_file <- system.file("rmarkdown/templates/accreditation/skeleton/skeleton.Rmd", package = "labeleR")
+  file.copy(tmpl_file, file.path(tmp, "accreditation.Rmd"), overwrite = TRUE)
+  tmpl_file   <- file.path(tmp, "accreditation.Rmd")
+  output_file <- 'Accreditations.pdf'  # allow user to choose name of the pdf file?
 
 
   rmarkdown::render(
-                    tmpl_file,
-                    output_dir = path,
-                    output_file = output_file,
-  params = list(
-    event        = event,
-    names        = data[,name.column],
-    affiliations = data[,affiliation.column]
-  ))
-
-  # file.copy(paste0("tmp/",output_file), paste0("output/",output_file), overwrite = T)#create files to call them lpic@rpic to make it homogeneous
-  unlink("tmp", recursive = T, force = T)
+    tmpl_file,
+    output_dir = path,
+    output_file = output_file,
+    params = list(
+      event        = event,
+      names        = data[, name.column],
+      affiliations = data[, affiliation.column]
+    )
+  )
 
 }
 
