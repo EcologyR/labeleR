a7c93b3a62bb3809f676f96f9ad44aab4792c467
Francisco Rodriguez-Sanchez <f.rodriguez.sanc@gmail.com>
2023-10-19 22:41:22 +0200

    remove startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,17 +1,4 @@
 
-.onAttach <- function(lib, pkg)
-{
-  packageStartupMessage(
-    cat(" \033[90mWelcome to the\033[39m \033[4m\033[1mlabeleR\033[22m\033[24m \033[90mpackage.\033[39m\n\n",
-           "\033[3mPackage developed by Ignacio Ramos-Gutiérrez, Julia G. de Aledo & Francisco Rodríguez-Sánchez\033[23m\n\n",
-           "\U1F426","/","\U1D54F", "@iramosgutierrez","\n",
-           "\U1F426","/","\U1D54F", "@juliagdealedo", "\n",
-           "\U1F426","/","\U1D54F", "@frod_san","\n")
-    )
-}
-
-
-
 
 #### Check columns in data
 
