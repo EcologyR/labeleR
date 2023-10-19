9cf70513b42ea6d5a4e9e334f4a9327f7a99262f
Francisco Rodriguez-Sanchez <f.rodriguez.sanc@gmail.com>
2023-10-19 13:11:09 +0200

    delete old test

--- tests/testthat/test-read_sheet.R
+++
@@ -1,8 +0,0 @@
-
-test_that("Check if URL is wrong", {
-  expect_error(read_sheet(url='https://docs.google.com/spreadsvUNzFk/edit#gid=0', "Certificado", "no"))
-})
-
-test_that("Check if selection values are OK", {
-expect_error(read_sheet(url='https://docs.google.com/spreadsheets/d/1uwhi7IROqDcdMEEld7yuyyAoSfuuJUbK2HVlHvUNzFk/edit#gid=0', "Certificado", "1"))
-})
