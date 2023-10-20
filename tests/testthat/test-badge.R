416af4d9a63e1d9c0c8bb8601545a3cdd9187118
Francisco Rodriguez-Sanchez <f.rodriguez.sanc@gmail.com>
2023-10-20 17:56:51 +0200

    test on GA?

--- tests/testthat/test-badge.R
+++ tests/testthat/test-badge.R
@@ -6,7 +6,6 @@ path <- tempdir()
 test_that("PDF file is created in the provided path", {
 
   skip_on_cran()
-  skip_on_ci()
 
   create_badge(data, path = path, name.column = "Names", affiliation.column = "Affil")
   expect_true(file.exists(file.path(path, "Badges.pdf")))
